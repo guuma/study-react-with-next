c7837e9d7be5dea34012df36a1abd08c12976c41
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-03-28 18:52:28 +0900

    ✨ Add about page

---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Footer from '../components/Footer';
+import Links from '../components/Links';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>About Page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          About<a href="https://nextjs.org">Page</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing <code className={styles.code}>pages/index.js</code>
+        </p>
+        <Links />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
