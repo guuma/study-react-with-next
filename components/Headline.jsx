dab1da4aa444cd01f664250ee5f4bd1b7af991df
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-03-29 15:30:46 +0900

    ♻️ Componentize the headline

---
+++ components/Headline.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+function Headline(props) {
+  console.log(props)
+  return (
+    <div>
+      <h1 className={styles.title}>
+        {`${props.page}`} Page
+      </h1>
+      <p className={styles.description}>
+        Get started by editing
+        <code className={styles.code}>{`pages/${props.page}.js`}</code>
+      </p>
+    </div>
+  );
+}
+
+export default Headline
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Footer from '../components/Footer';
 import Links from '../components/Links';
+import Headline from '../components/Headline';
 
 export default function Home() {
   return (
@@ -12,13 +13,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Index<a href="https://nextjs.org">Page</a>
-        </h1>
+        <Headline page="Index"/>
         <Links />
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
       </main>
 
       <Footer />
