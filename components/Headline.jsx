56195bf68b2172e315f62ce74e8b6313548083d8
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-06-20 21:54:00 +0900

    Modified Headline.jsx and Rename about.jsx and index.jsx

--- components/Headline.jsx
+++ components/Headline.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 export function Headline(props) {
   return (
     <div>
-      <h1 className={styles.title}>{`${props.page}`} Page</h1>
+      <h1 className={styles.title}>{`${props.page} Page`}</h1>
       <p className={styles.description}>
         Get started by editing
         {props.children}
