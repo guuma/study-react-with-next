85abc39bb98c6b23432497dd207f84ad3f875616
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-06-24 22:38:51 +0900

    Add useEffect pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import Head from 'next/head';
 import classes from '../styles/Home.module.css';
 import { Footer } from '../components/Footer';
@@ -6,6 +6,14 @@ import { Header } from '../components/Header';
 import { Main } from '../components/Main';
 
 export default function Home() {
+  useEffect(() => {
+    console.log('Component Did Mount');
+    document.body.style.backgroundColor = 'lightblue';
+    return () => {
+      console.log('Unmount');
+      document.body.style.backgroundColor = '';
+    };
+  }, []);
   const handleClick = useCallback((e) => {
     console.log(e.target.href);
     e.preventDefault();
